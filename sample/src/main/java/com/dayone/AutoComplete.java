449af5fa1199656f0cfeeef5c1065303b5fc1ddb
minmul10 <51876783+minmul10@users.noreply.github.com>
2022-09-25 16:32:39 +0900

    Initial commit

---
+++ sample/src/main/java/com/dayone/AutoComplete.java
@@ -0,0 +1,12 @@
+package com.dayone;
+
+import org.apache.commons.collections4.Trie;
+
+public class AutoComplete {
+    private Trie trie;
+
+    public AutoComplete(Trie trie) {
+        this.trie = trie;
+    }
+
+}
